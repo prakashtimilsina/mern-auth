064d2517e97f26b22c1148fc3a913caf385812a2
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-02-17 22:18:19 -0600

    Header Section is created

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='bg-teal-100'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>
+            <h1 className='font-bold'>Auth App</h1>
+            </Link>
+            <ul className='flex gap-2 sm:gap-10'>
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/about'><li>About</li></Link>
+                <Link to='/sign-in'><li>SignIn</li></Link>            
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
