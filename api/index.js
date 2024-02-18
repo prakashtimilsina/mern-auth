f2f6dd80659870351965fb8b660c1cbe9994adae
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-02-18 00:01:11 -0600

    api routes, usermodels, user signup are added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,24 @@
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
-app.listen(3000, ()=>{
-    console.log('Server is listening on port 3000')
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URL)
+        .then(()=>{console.log('MongoDB is connected Successfully.')})
+        .catch((err)=>{console.error('MongoDB Connection is failed.', err.message)});
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`)
 })
+
+
+//API ROUTES
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
