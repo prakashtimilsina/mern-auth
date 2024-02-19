68e6dc8f65da76b485a18bfd790132e2b171a2b8
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-02-19 00:00:45 -0600

    Signin API is added.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import {errorHandler} from '../utils/error.js'
 
 export const signup = async (req, res, next)=>{
@@ -12,4 +13,31 @@ export const signup = async (req, res, next)=>{
     } catch (error) {
         next(error);
     }
+}
+
+export const signin = async(req, res, next)=>{
+    const { email, password } = req.body;
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, "All fields are required."))
+    }
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'email/password1 is not valid'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401, 'email/password2 is not valid'));
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.TOKEN_SECRET_KEY, {expiresIn: '1d'});
+        //Removing password from user data
+        const filterUser = await User.findById(validUser._id).select("-password");
+        
+        res.status(200)
+            .cookie('access_token', token, {httpOnly : true})
+            .json({user: filterUser, message: "Signin Successful."})
+    } catch (error) {
+       next(error) 
+    }
+
 }
